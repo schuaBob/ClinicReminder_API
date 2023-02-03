f2a52d926e970c578d8385452e00b04bf5b54b38
Bob Hua <schua1013@gmail.com>
2023-02-03 05:45:51 -0500

    create requestresponsefilter template

---
+++ src/main/java/com/huaclinic/restfulapi/filters/RequestResponseFilter.java
@@ -0,0 +1,40 @@
+package com.huaclinic.restfulapi.filters;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import java.io.IOException;
+import org.springframework.web.filter.GenericFilterBean;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * RequestResponseFilter
+ */
+@Configuration
+public class RequestResponseFilter {
+    @Bean
+    public CommonsRequestLoggingFilter RequestFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+
+    @Component
+    public class ResponseFilter extends GenericFilterBean {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+            HttpServletResponse res = (HttpServletResponse) response;
+            chain.doFilter(request, response);
+            System.out.println(res.getContentType());
+        }
+    }
+}
