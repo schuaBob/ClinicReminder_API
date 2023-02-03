dbcbf0f71ffd91670b603579479ad49d4b785155
Bob Hua <schua1013@gmail.com>
2023-02-03 02:42:11 -0500

    init

---
+++ src/test/java/com/huaclinic/restfulapi/RestfulapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huaclinic.restfulapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
