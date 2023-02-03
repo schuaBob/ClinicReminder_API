fd4e3754b7aecc0a4a347312ffdea930c669a295
Bob Hua <schua1013@gmail.com>
2023-02-03 04:10:48 -0500

    remove login

--- src/main/java/com/huaclinic/restfulapi/config/RequestLoggingFilterConfig.java
+++ src/main/java/com/huaclinic/restfulapi/config/RequestLoggingFilterConfig.java
@@ -13,7 +13,7 @@ public class RequestLoggingFilterConfig {
         filter.setIncludePayload(true);
         filter.setMaxPayloadLength(10000);
         filter.setIncludeHeaders(false);
-        filter.setAfterMessagePrefix("REQUEST DATA: ");
         return filter;
     }
+    
 }
